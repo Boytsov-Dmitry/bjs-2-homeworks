d9efbb6636c53e21c07141853f9c3e0b73c6db40
Dmitry Boytsov <135224601+Boytsov-Dmitry@users.noreply.github.com>
2023-09-08 09:10:22 +0300

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,16 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
-  
+  let arr = [b**-4*a*c];
+  if (arr < 0) {
+    arr = [];
+    console.log(arr); 
+  }
+  if (arr = 0) {
+    console.log(-b/(2*a))
+  }
+  if (arr > 0) {
+    console.log((-b + Math.sqrt(d) )/(2*a), (-b - Math.sqrt(d) )/(2*a));
+  }
   return arr;
 }
 
