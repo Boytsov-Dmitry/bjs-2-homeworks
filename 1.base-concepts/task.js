c06933e214e7c5edc5e2ce69660a2f77ac0680f7
Boytsov Dmitry <bdo5677905@yandex.ru>
2023-09-10 13:34:28 +0300

    First try

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,19 +1,31 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
-  let arr = [b**-4*a*c];
-  if (arr < 0) {
+  let d = b** - 4 * a * c;
+  let arr = [];
+  if (d < 0) {
     arr = [];
-    console.log(arr); 
   }
-  if (arr = 0) {
-    console.log(-b/(2*a))
+
+  if (d === 0) {
+    arr = [-b / (2 * a)];
   }
-  if (arr > 0) {
-    console.log((-b + Math.sqrt(d) )/(2*a), (-b - Math.sqrt(d) )/(2*a));
+
+  if (d > 0) {
+    arr = [((-b + Math.sqrt(d) ) / (2 * a)), ((-b - Math.sqrt(d) ) / (2 * a))];
   }
+  
   return arr;
 }
 
+solveEquation(1, 5, 4)
+
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  percent = percent * 0.01 / 12; 
+  let creditBody = amount - contribution;
+  let paymentPerMonth = creditBody * (percent + (percent / (((1 + percent)**countMonths) - 1)));
+  let fullPayment = Number((paymentPerMonth * countMonths).toFixed(2));
+  console.log(fullPayment);
 }
+
+calculateTotalMortgage(10, 1000, 50000, 12)
