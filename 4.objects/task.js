8c84eb1b9e9cf6a536d6057d16a4b674937eeb15
Boytsov Dmitry <bdo5677905@yandex.ru>
2023-09-21 17:24:57 +0300

    4 ready

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,47 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.subject = '';
+    this.marks = [];
+    this.excluded = '';
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+let student1 = new Student('Valli', 'male', 30);
+
+let student2 = new Student("Артём", "мужской", 25);
+
+let student3 = new Student('Grolli', 'male', 18);
+
+
+Student.prototype.setSubject = function (subjectName) { 
+    this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+student2.setSubject("Geometry");
+
+Student.prototype.addMarks = function (...marksToAdd) {
+    if (this.hasOwnProperty('marks') === true) {
+        this.marks.push(...marksToAdd)
+    }
 }
+
+student2.addMarks();
 
 Student.prototype.getAverage = function () {
-  
+    if(this.hasOwnProperty('marks') === true) {
+        return this.marks.reduce((acc, item, index, marks) => acc + item / marks.length, 0)  
+    } else {
+        return 0
+    }
 }
 
+console.log(student1.getAverage())
+
 Student.prototype.exclude = function (reason) {
-  
-}
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
+  }
+
+  student2.exclude('плохая учёба')
